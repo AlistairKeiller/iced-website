0ab77ff110444b7cfd028f274549e1e57413401e
Alistair Keiller <alistair@keiller.net>
2022-10-08 21:48:56 -0700

    add a basic main page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<div class="mx-auto mt-20 flex max-w-5xl flex-col space-y-8 text-center lg:mt-32">
+	<div class="text-5xl font-bold text-slate-900 dark:text-slate-100 md:text-7xl">
+		<!-- A cross-platform <span class="text-red-400">Rust</span> GUI library focused on simplicity and type-safety -->
+		Build type-safe cross-platform GUI applications in <span class="text-red-400">Rust</span>
+	</div>
+	<div class="flex justify-center space-x-6">
+		<a href="https://docs.rs/iced/" class="rounded-lg bg-sky-500 px-4 py-3 font-semibold text-white hover:bg-sky-400">Get Started</a>
+		<a href="https://docs.rs/iced/" class="rounded-lg bg-slate-800 px-4 py-3 font-semibold text-white hover:bg-slate-700">Contribute</a>
+	</div>
+</div>
+
+<!-- <div class="flex">test</div> -->
+
+<!-- <div><span class="text-sky-400">Iced</span> is inspired by the Elm architecture which gives it a simplicity that anyone can appreciate. <br /> <span class="text-sky-400">Iced</span>'s wgpu backend supports Vulkan, Metal and DX12, allowing it to be performant on any platform.</div> -->
+
+<!-- <img src="src/routes/ecosystem.png" alt="diagram of iced's architecture" class="mx-auto mt-8" /> -->
