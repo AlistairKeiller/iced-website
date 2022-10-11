5fd31f15dc5d250acdef7eb3e5bdd0a2eb46ad35
Alistair Keiller <alistair@keiller.net>
2022-10-10 22:51:42 -0700

    update features

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,21 +14,26 @@
 	</div>
 </div>
 
-<div class="mx-auto mt-16 flex max-w-7xl flex-col items-center justify-between fill-slate-700 text-center dark:fill-slate-200 lg:flex-row">
-	<div class="mt-5 flex max-w-sm flex-col">
+<div class="mx-auto mt-16 grid max-w-7xl auto-cols-auto grid-cols-1 gap-5 fill-slate-700 text-center dark:fill-slate-200 md:grid-cols-2">
+	<div class="flex flex-col">
 		<svg height="128" viewBox="0 0 48 48"><path d="m17.3 45-3.8-6.5-7.55-1.55.85-7.35L2 24l4.8-5.55-.85-7.35 7.55-1.55L17.3 3 24 6.1 30.7 3l3.85 6.55 7.5 1.55-.85 7.35L46 24l-4.8 5.6.85 7.35-7.5 1.55L30.7 45 24 41.9Zm1.35-3.95L24 38.8l5.5 2.25 3.35-5 5.85-1.5-.6-5.95 4.05-4.6-4.05-4.7.6-5.95-5.85-1.4-3.45-5L24 9.2l-5.5-2.25-3.35 5-5.85 1.4.6 5.95L5.85 24l4.05 4.6-.6 6.05 5.85 1.4ZM24 24Zm-2.15 6.65L33.2 19.4l-2.25-2.05-9.1 9-4.75-4.95-2.3 2.25Z" /></svg>
-		<div class="text-5xl text-slate-700 dark:text-slate-100">Simple</div>
-		Iced is inspired by the Elm architecture, which gives it a simplicity that anyone can appreciate.
+		<div class="text-5xl text-slate-700 dark:text-slate-100">Easy-to-use</div>
+		Batteries-included API to get started quickly and create your own widgets easily.
 	</div>
-	<div class="mt-5 flex max-w-sm flex-col">
+	<div class="flex flex-col">
 		<svg height="128" viewBox="0 0 48 48"><path d="M11 44q-1.25 0-2.125-.875T8 41V19.3q0-1.25.875-2.125T11 16.3h3.5v-4.8q0-3.95 2.775-6.725Q20.05 2 24 2q3.95 0 6.725 2.775Q33.5 7.55 33.5 11.5v4.8H37q1.25 0 2.125.875T40 19.3V41q0 1.25-.875 2.125T37 44Zm0-3h26V19.3H11V41Zm13-7q1.6 0 2.725-1.1t1.125-2.65q0-1.5-1.125-2.725T24 26.3q-1.6 0-2.725 1.225T20.15 30.25q0 1.55 1.125 2.65Q22.4 34 24 34Zm-6.5-17.7h13v-4.8q0-2.7-1.9-4.6Q26.7 5 24 5q-2.7 0-4.6 1.9-1.9 1.9-1.9 4.6ZM11 41V19.3 41Z" /></svg>
-		<div class="text-5xl text-slate-700 dark:text-slate-100">Type Safe</div>
-		So you can debug errors in devopment instead of in production.
+		<div class="text-5xl text-slate-700 dark:text-slate-100">Type-Safe</div>
+		Iced uses reactive programming model to ensure your UI is meaningful.
 	</div>
-	<div class="mt-5 flex max-w-sm flex-col">
-		<svg height="128" viewBox="0 0 48 48"><path d="M20.35 31.75q1.15 1.15 3.325 1.075 2.175-.075 3.175-1.625l10.8-16.95L20.9 25.2q-1.5 1-1.6 3.2-.1 2.2 1.05 3.35Zm3.55-23.7q2.85 0 5.95.925t5.95 3.175L33.2 14q-2.25-1.5-4.825-2.225-2.575-.725-4.475-.725-7 0-11.95 5Q7 21.05 7 28.2q0 2.25.625 4.55T9.4 37h28.95q1.1-1.8 1.75-4.2.65-2.4.65-4.7 0-2.1-.625-4.525Q39.5 21.15 37.9 19.1l1.95-2.6q1.9 2.8 2.85 5.625.95 2.825 1.05 5.675.1 3-.6 5.65-.7 2.65-2.05 4.9-.6 1.15-1.275 1.4-.675.25-1.675.25H9.6q-.85 0-1.675-.425Q7.1 39.15 6.7 38.35q-1.3-2.4-2-4.875T4 28.2q0-4.15 1.575-7.825t4.275-6.4q2.7-2.725 6.325-4.325t7.725-1.6Zm-.45 16.55Z" /></svg>
-		<div class="text-5xl text-slate-700 dark:text-slate-100">Performant</div>
-		wgpu supports Vulkan, Metal and DX12, so iced will have a performant backend on any platform.
+	<div class="flex flex-col">
+		<svg height="128" viewBox="0 0 48 48"><path d="M4 40v-4.5h4.3V11q0-1.25.875-2.125T11.3 8h31v3h-31v24.5H23V40Zm23.95 0q-.85 0-1.4-.675Q26 38.65 26 37.8V15.95q0-.85.55-1.4.55-.55 1.4-.55h13.6q.95 0 1.7.525T44 15.95V37.8q0 .95-.725 1.575T41.55 40ZM29 35.5h12V17H29Z" /></svg>
+		<div class="text-5xl text-slate-700 dark:text-slate-100">Cross-platform</div>
+		Custom renderers for every context, from native to the web.
+	</div>
+	<div class="flex flex-col">
+		<svg height="128" viewBox="0 0 48 48"><path d="M6 22.5V6h16.5v16.5ZM6 42V25.5h16.5V42Zm19.5-19.5V6H42v16.5Zm0 19.5V25.5H42V42ZM9 19.5h10.5V9H9Zm19.5 0H39V9H28.5Zm0 19.5H39V28.5H28.5ZM9 39h10.5V28.5H9Zm19.5-19.5Zm0 9Zm-9 0Zm0-9Z" /></svg>
+		<div class="text-5xl text-slate-700 dark:text-slate-100">Modular</div>
+		Iced ecosystem is split into reusable parts.
 	</div>
 </div>
 
