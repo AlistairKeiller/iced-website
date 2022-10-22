4f0f3d4c13b5be19ac1edeab5dca2bf28a1c2d8c
Alistair Keiller <alistair@keiller.net>
2022-10-22 15:09:58 -0700

    add sidebar button

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 import Unocss from 'unocss/vite';
+import { extractorSvelte } from '@unocss/core';
 import presetIcons from '@unocss/preset-icons';
 import presetUno from '@unocss/preset-uno';
 import presetTypography from '@unocss/preset-typography';
 
 const config: UserConfig = {
-	plugins: [sveltekit(), Unocss({ presets: [presetUno(), presetIcons(), presetTypography()] })]
+	plugins: [sveltekit(), Unocss({ extractors: [extractorSvelte], presets: [presetUno(), presetIcons(), presetTypography()] })]
 };
 
 export default config;
